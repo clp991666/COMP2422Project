9a601e676bb333d8a3467a9a8caac58f2a1bece5
lkho <llhmtc@gmail.com>
2015-04-10 01:07:19 +0800

    increase db field length

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `bookings` (
   `user` varchar(20) NOT NULL,
   `time` datetime NOT NULL,
-  `activity` varchar(50) NOT NULL,
-  `venue` varchar(50) NOT NULL,
-  `court` varchar(50) NOT NULL,
+  `activity` varchar(255) NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `court` varchar(255) NOT NULL,
   PRIMARY KEY (`time`,`activity`,`venue`,`court`),
   KEY `IX_time` (`time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
