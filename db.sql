96dbb6a61d7d3fa620c5b794f1f185f208feb075
lkho <llhmtc@gmail.com>
2015-04-12 21:24:45 +0800

    add foreign key

--- db.sql
+++ db.sql
@@ -6,13 +6,16 @@ CREATE TABLE `bookings` (
   `court` varchar(255) NOT NULL,
   `fee` decimal(10,2) NOT NULL default 0,
   PRIMARY KEY  (`time`,`activity`,`venue`,`court`),
-  KEY `IX_time` (`time`)
+  KEY `IX_time` (`time`),
+  KEY `FK_users_idx` (`user`),
+  CONSTRAINT `FK_users` FOREIGN KEY (`user`) REFERENCES `comp2422_users` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `users` (
   `id` varchar(20) NOT NULL,
   `pw` varchar(255) default NULL,
   `name` varchar(255) default NULL,
-  `deposit` decimal(10,2) NOT NULL,
+  `deposit` decimal(10,2) NOT NULL default 0,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
