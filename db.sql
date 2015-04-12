ee79387cd8ecb6c5268b0876fe3eea4319a2b89f
lkho <llhmtc@gmail.com>
2015-04-12 21:02:30 +0800

    update db schema

--- db.sql
+++ db.sql
@@ -4,9 +4,11 @@ CREATE TABLE `bookings` (
   `activity` varchar(255) NOT NULL,
   `venue` varchar(255) NOT NULL,
   `court` varchar(255) NOT NULL,
-  PRIMARY KEY (`time`,`activity`,`venue`,`court`),
+  `fee` decimal(10,2) NOT NULL default 0,
+  PRIMARY KEY  (`time`,`activity`,`venue`,`court`),
   KEY `IX_time` (`time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `users` (
   `id` varchar(20) NOT NULL,
   `pw` varchar(255) default NULL,
